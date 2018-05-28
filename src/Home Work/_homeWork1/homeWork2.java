1433b1171b3bf8386e1cae51ed5d6efbc878dd69
ashcherbyna <ashcherbyna@playtika.com>
2018-05-28 14:09:24 +0300

    Second DZ2

---
+++ src/Home Work/_homeWork1/homeWork2.java
@@ -0,0 +1,15 @@
+public class homeWork2 {
+    public static void main(String[] args) {
+        int number = 54321;
+
+        int a = number / 10000;
+        int b = ( number - a * 10000) / 1000;
+        int c = ( number - a * 10000 - b * 1000 ) / 100;
+        int d = ( number - a * 10000 - b * 1000 - c * 100 ) /10;
+        int e = ( number - a * 10000 - b * 1000 - c *100 - d *10);
+
+        System.out.printf("Number consists of numbers :%n%d%n%d%n%d%n%d%n%d",a,b,c,d,e);
+
+    }
+}
+
