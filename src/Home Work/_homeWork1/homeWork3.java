7e9272ca3d3e11866b003b16c6bec28c34ae8a02
ashcherbyna <ashcherbyna@playtika.com>
2018-05-28 14:24:39 +0300

    Third DZ3

---
+++ src/Home Work/_homeWork1/homeWork3.java
@@ -0,0 +1,20 @@
+public class homeWork3 {
+    public static void main(String[] args) {
+        byte X = 1;
+        byte Y = 2;
+        byte Z = 3;
+
+        System.out.println(calculationVector(X,Y,Z));
+    }
+
+    private static String  calculationVector(byte X, byte Y, byte Z) {
+        double vectorLenth = (Math.sqrt( X * X + Y * Y + Z * Z ) );
+        double firstVector = ( X / vectorLenth );
+        double secondVector = ( Y / vectorLenth );
+        double thirdVector = ( Z / vectorLenth );
+
+        return new String("Unit Vector of numbers (" +X+", "+Y+", "+Z+") is (" +firstVector+", "+secondVector+", "+thirdVector+")");
+
+
+    }
+}
